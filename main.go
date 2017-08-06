1c218b9ad35f5fd3a2bfc1016165f19e93184529
Valery Kalashnikov <kalashnikov.valeri@gmail.com>
2017-08-06 18:22:03 +0300

    Step 1. Meta is necessary.

---
+++ main.go
@@ -0,0 +1,21 @@
+// go-swagger examples.
+//
+// The purpose of this application is to provide some
+// use cases describing how to generate docs for your API
+//
+//     Schemes: http, https
+//     Host: localhost
+//     BasePath: /
+//     Version: 0.0.1
+//
+//     Consumes:
+//     - application/json
+//
+//     Produces:
+//     - application/json
+//
+// swagger:meta
+package main
+
+func main() {
+}
